106afa3301490a2d81dcf62c19cf96a5694c15e7
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2022-05-01 09:43:23 +0530

    Update AutheticateAspect.java

--- src/main/java/com/example/demo/aspect/AutheticateAspect.java
+++ src/main/java/com/example/demo/aspect/AutheticateAspect.java
@@ -30,17 +30,20 @@ public class AutheticateAspect {
     @Around("@annotation(com.example.demo.annotation.Autheticate)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 
+        // add request with autowired annotation
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         String token = request.getHeader("Authorization");
 
+        // take secret from applicatiom.yml from resources
         String secret = "bhagyesh";
+        // add this logic to utils with singleton design pattern
         Key key = new SecretKeySpec(Base64.getDecoder().decode(secret), SignatureAlgorithm.HS256.getJcaName());
 
         try {
             Jws<Claims> jwt = Jwts.parser()
             .setSigningKey(key)
             .parseClaimsJws(token);
-
+ 
             request.setAttribute("email", jwt.getBody().get("email"));
 
             Object proceed = joinPoint.proceed();
