6e69ecc703abb0747b0fda81bb6bfffb613d10c0
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2022-05-02 15:23:58 +0530

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,19 +1,12 @@
 package com.example.demo.controller;
 
-import java.security.Key;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Base64;
-import java.util.Date;
 import java.util.Optional;
-import java.util.UUID;
-
-import javax.crypto.spec.SecretKeySpec;
 import javax.validation.Valid;
 
 import com.example.demo.entity.User;
 import com.example.demo.pojo.SignupRequest;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.utils.Utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,9 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -32,6 +22,9 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils util;
+	
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@Valid @RequestBody SignupRequest request) {
         if(userRepository.existsByEmail(request.getEmail())) {
@@ -50,22 +43,10 @@ public class UserController {
         if (!user.isPresent()) {
             return ResponseEntity.badRequest().body("email not found!");
 	}
-	// update
-	String secret = "bhagyesh";
-        Key key = new SecretKeySpec(Base64.getDecoder().decode(secret), SignatureAlgorithm.HS256.getJcaName());
-
-        Instant now = Instant.now();
-        // compare with encoded password
+	// compare with encoded password
 	// String encoded = new BCryptPasswordEncoder().encode(plainTextPassword);
         if (user.get().getPassword().equals(request.getPassword())) {
-            String jwtToken = Jwts.builder()
-                .claim("email", request.getEmail()) // pass user object
-                .setSubject("bhagyesh") //remove
-                .setId(UUID.randomUUID().toString()) // remove
-                .setIssuedAt(Date.from(now))
-                .setExpiration(Date.from(now.plus(10, ChronoUnit.MINUTES)))
-                .signWith(SignatureAlgorithm.HS256, key)
-                .compact();
+            String jwtToken = util.generateToken(user.get().getId());
             return ResponseEntity.ok(jwtToken);
         } else {
             return ResponseEntity.ok("failed!"); //update response 
