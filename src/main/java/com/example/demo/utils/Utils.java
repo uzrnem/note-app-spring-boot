db2f755fead8069dadecc97d35be36306df98a92
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2022-05-02 13:07:53 +0530

    Update Utils.java

--- src/main/java/com/example/demo/utils/Utils.java
+++ src/main/java/com/example/demo/utils/Utils.java
@@ -1,10 +1,24 @@
 package com.example.demo.utils;
 
+import java.security.Key;
+import java.util.Base64;
+import javax.crypto.spec.SecretKeySpec;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class Utils {
   
+  @Value("${spring.jwt.secret}")
+  String secret;
+
+  private Key key;
   
-  
+  public Utils() {
+    if (myStr2.isEmpty()) {
+      secret = "default";
+    }
+    key = new SecretKeySpec(Base64.getDecoder().decode(secret), SignatureAlgorithm.HS256.getJcaName());
+  }
 }
