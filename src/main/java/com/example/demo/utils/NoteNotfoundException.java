219a0ef7b5ef2829848b38390ed63c5fb0dffd6d
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2022-05-02 12:45:40 +0530

    Update and rename src/main/java/com/example/demo/interfaces/NoteNotfoundException.java to src/main/java/com/example/demo/utils/NoteNotfoundException.java

--- src/main/java/com/example/demo/utils/NoteNotfoundException.java
+++ src/main/java/com/example/demo/utils/NoteNotfoundException.java
@@ -1,4 +1,4 @@
-package com.example.demo.interfaces;
+package com.example.demo.utils;
 
 public class NoteNotfoundException extends RuntimeException {
    private static final long serialVersionUID = 1L;
