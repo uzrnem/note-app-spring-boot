0bfe8f3da849f02e8b17bf8c84493bfda837da76
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2022-05-02 14:05:57 +0530

    Update Utils.java

--- src/main/java/com/example/demo/utils/Utils.java
+++ src/main/java/com/example/demo/utils/Utils.java
@@ -4,26 +4,46 @@ import java.security.Key;
 import java.util.Base64;
 import javax.crypto.spec.SecretKeySpec;
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 
 @Component
 public class Utils {
   
   @Value("${spring.jwt.secret}")
   String secret;
+  
+  @Value("${spring.jwt.exp_time}")
+  Integer expTime;
 
   private Key key;
   
-  public Utils() {
-    if (myStr2.isEmpty()) {
-      secret = "default";
+  public Key getKey() {
+    if (key == null) {
+      key = new SecretKeySpec(Base64.getDecoder().decode(secret), SignatureAlgorithm.HS256.getJcaName());
     }
-    key = new SecretKeySpec(Base64.getDecoder().decode(secret), SignatureAlgorithm.HS256.getJcaName());
+    return key;
   }
   
-  public Key getJwtKey() {
-    return this.key;
+  public String generateKey(Integer userId) {
+    return Jwts.builder()
+                .claim("userId", userId)
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(now.plus(expTime, ChronoUnit.SECONDS)))
+                .signWith(SignatureAlgorithm.HS256, this.getKey())
+                .compact();
+  }
+  
+  public String validateKey(String token) throws Throwable {
+    Jws<Claims> jwt = Jwts.parser()
+            .setSigningKey(this.getKey())
+            .parseClaimsJws(token);
+    return jwt.getBody().get("userId"));
   }
 }
