a23bd6b6f0a234768b0f005cf080465464d754e2
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2022-05-02 12:38:52 +0530

    Update AutheticateAspect.java

--- src/main/java/com/example/demo/aspect/AutheticateAspect.java
+++ src/main/java/com/example/demo/aspect/AutheticateAspect.java
@@ -14,6 +14,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.web.context.request.RequestContextHolder;
 // import org.springframework.web.context.request.ServletRequestAttributes;
 
