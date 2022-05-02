adf5741be2c6ea6f7d3356a47d7f3e1aceb62124
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2022-05-02 15:28:01 +0530

    Update AutheticateAspect.java

--- src/main/java/com/example/demo/utils/AutheticateAspect.java
+++ src/main/java/com/example/demo/utils/AutheticateAspect.java
@@ -1,13 +1,7 @@
 package com.example.demo.utils;
 
-import java.security.Key;
-import java.util.Base64;
-
-import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
-
 import com.fasterxml.jackson.core.io.JsonEOFException;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -16,12 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureAlgorithm;
 
 @Aspect
 @Component
@@ -30,22 +20,16 @@ public class AutheticateAspect {
     @Autowired
     HttpServletRequest request;
 
+    @Autowired
+    Utils util;
+
     @Around("@annotation(com.example.demo.interfaces.Autheticate)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 
         String token = request.getHeader("Authorization");
 
-        // take secret from applicatiom.yml from resources
-        String secret = "bhagyesh";
-        // add this logic to utils with singleton design pattern
-        Key key = new SecretKeySpec(Base64.getDecoder().decode(secret), SignatureAlgorithm.HS256.getJcaName());
-
         try {
-            Jws<Claims> jwt = Jwts.parser()
-            .setSigningKey(key)
-            .parseClaimsJws(token);
- 
-            request.setAttribute("email", jwt.getBody().get("email"));
+            request.setAttribute("userId", util.validateToken(token));
 
             Object proceed = joinPoint.proceed();
 
