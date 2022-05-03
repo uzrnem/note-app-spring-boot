679e931859070e44d770c247ac41470b7fd9db02
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2022-05-03 08:39:59 +0530

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,6 +7,7 @@ import com.example.demo.entity.User;
 import com.example.demo.schema.SignRequest;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.utils.Utils;
+import com.example.demo.schema.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +15,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.media.*;
+import io.swagger.v3.oas.annotations.responses.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "user", description = "the User API")
 public class UserController {
 
     @Autowired
@@ -26,30 +32,28 @@ public class UserController {
     Utils util;
 	
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@Valid @RequestBody SignRequest request) {
+    public ResponseEntity<Response<String>> signup(@Valid @RequestBody SignRequest request) {
         if(userRepository.existsByEmail(request.getEmail())) {
-            return ResponseEntity.badRequest().body("email exists!");
+            return ResponseEntity.badRequest().body(new Response<>("email exists!", null));
         }
         // encode password
-	// String encoded = new BCryptPasswordEncoder().encode(plainTextPassword);
+        // String encoded = new BCryptPasswordEncoder().encode(plainTextPassword);
         User user = new User(request.getEmail(), request.getPassword());
         userRepository.save(user);
-        return ResponseEntity.ok("saved!");
+        return ResponseEntity.ok(new Response<>("registration successfull!", util.generateToken(user)));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody SignRequest request) {
+    public ResponseEntity<Response<String>> login(@Valid @RequestBody SignRequest request) {
         Optional<User> user = userRepository.findByEmail(request.getEmail());
         if (!user.isPresent()) {
-            return ResponseEntity.badRequest().body("email not found!");
-	}
-	// compare with encoded password
-	// String encoded = new BCryptPasswordEncoder().encode(plainTextPassword);
+            return ResponseEntity.badRequest().body(new Response<>("email not found!", null));
+        }
         if (user.get().getPassword().equals(request.getPassword())) {
             String jwtToken = util.generateToken(user.get().getId());
-            return ResponseEntity.ok(jwtToken);
+            return ResponseEntity.ok(new Response<>("login successfull!", util.generateToken(user.get())));
         } else {
-            return ResponseEntity.ok("failed!"); //update response 
+            return ResponseEntity.badRequest().body(new Response<>("wrong password!", null));
         }
     }
 
