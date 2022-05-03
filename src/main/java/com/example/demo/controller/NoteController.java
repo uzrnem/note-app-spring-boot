d8a5bc71774cbcef5267beb01d20b4a6850e3637
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2022-05-03 17:39:33 +0530

    Update NoteController.java

--- src/main/java/com/example/demo/controller/NoteController.java
+++ src/main/java/com/example/demo/controller/NoteController.java
@@ -1,36 +1,36 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import com.example.demo.entity.Note;
+import com.example.demo.entity.User;
 import com.example.demo.interfaces.Autheticate;
 import com.example.demo.repository.NoteRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.schema.NoteRequest;
 import com.example.demo.schema.Response;
-import com.example.demo.entity.Note;
-import com.example.demo.entity.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.media.*;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/notes")
+@RequestMapping("/note")
 @Tag(name = "note", description = "the Note API")
 public class NoteController {
 
@@ -45,87 +45,117 @@ public class NoteController {
 
     @Autheticate
     @GetMapping()
-    @Operation(summary = "Get all notes for user", description = "Return list of notes", tags = { "note" })
+    @Operation(summary = "List of user notes", tags = { "note" })
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "successful operation"),
         @ApiResponse(responseCode = "401", description = "failed operation",
-                content = @Content(schema = @Schema(implementation = Response.class))) })
-    public ResponseEntity<Response<List<Note>>> list() {
+            content = @Content(schema = @Schema(implementation = Response.class))) })
+    public ResponseEntity<Response<List<Note>>> listNote() {
+        Optional<User> user = userRepository.findById((Integer)request.getAttribute("user_id"));
 
-        List<Note> list = noteRepository.getByUserId((Integer)request.getAttribute("user_id"));
-        return ResponseEntity.ok(new Response<>("Success", list));
+        if (!user.isPresent()) {
+            return ResponseEntity.badRequest().body(new Response<>("user not found!", null));
+        }
+
+        List<Note> notes = noteRepository.getByUserId(user.get().getId());
+
+        return ResponseEntity.ok(new Response<>("notes listed!", notes));
     }
 
     @Autheticate
     @PostMapping()
-    @Operation(summary = "Add Note to List", description = "create note", tags = { "note" })
+    @Operation(summary = "Add Note to User List", tags = { "note" })
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "successful operation"),
         @ApiResponse(responseCode = "401", description = "failed operation",
                 content = @Content(schema = @Schema(implementation = Response.class))) })
-    public ResponseEntity<Response<Note>> add(@Valid @RequestBody NoteRequest noteRequest) {
-
+    public ResponseEntity<Response<Note>> addNote(@Valid @RequestBody NoteRequest noteRequest) {
         Optional<User> user = userRepository.findById((Integer)request.getAttribute("user_id"));
+
         if (!user.isPresent()) {
-            return ResponseEntity.badRequest().body(new Response<>("email not found!", null));
+            return ResponseEntity.badRequest().body(new Response<>("user not found!", null));
         }
         Note note = new Note(noteRequest.getContent(), user.get(), noteRequest.getIsCompleted());
+
         noteRepository.save(note);
-        return ResponseEntity.ok(new Response<>("Success", note));
+        return ResponseEntity.ok(new Response<>("note created!", note));
     }
 
     @Autheticate
     @GetMapping("/{id}")
-    @Operation(summary = "Get note by id", description = "Return note of user", tags = { "note" })
+    @Operation(summary = "Fetch User Note", tags = { "note" })
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "successful operation"),
         @ApiResponse(responseCode = "401", description = "failed operation",
                 content = @Content(schema = @Schema(implementation = Response.class))) })
-    public ResponseEntity<Response<Note>> get(@PathVariable("id") Integer id) {
+    public ResponseEntity<Response<Note>> getNote(@PathVariable("id") Long id) {
+        Optional<User> user = userRepository.findById((Integer)request.getAttribute("user_id"));
 
-        Optional<Note> notePromise = noteRepository.getByIdAndUserId(id, (Integer)request.getAttribute("user_id"));
-        if (!notePromise.isPresent()) {
+        if (!user.isPresent()) {
+            return ResponseEntity.badRequest().body(new Response<>("user not found!", null));
+        }
+
+        Optional<Note> note = noteRepository.getByIdAndUserId(id, user.get().getId());
+
+        if (!note.isPresent()) {
             return ResponseEntity.badRequest().body(new Response<>("note not found!", null));
         }
-        Note note = notePromise.get();
-        return ResponseEntity.ok(new Response<>("Success", note));
+
+        return ResponseEntity.ok(new Response<>("note showed!", note.get()));
     }
 
     @Autheticate
     @PostMapping("/{id}")
-    @Operation(summary = "Update note by id", description = "Update note of user", tags = { "note" })
+    @Operation(summary = "Update User Note", tags = { "note" })
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "successful operation"),
         @ApiResponse(responseCode = "401", description = "failed operation",
                 content = @Content(schema = @Schema(implementation = Response.class))) })
-    public ResponseEntity<Response<Note>> update(@PathVariable("id") Integer id, @Valid @RequestBody NoteRequest noteRequest) {
+    public ResponseEntity<Response<Note>> updateNote(@PathVariable("id") Long id, @Valid @RequestBody NoteRequest noteRequest) {
+        Optional<User> user = userRepository.findById((Integer)request.getAttribute("user_id"));
 
-        Optional<Note> notePromise = noteRepository.getByIdAndUserId(id, (Integer)request.getAttribute("user_id"));
-        if (!notePromise.isPresent()) {
+        if (!user.isPresent()) {
+            return ResponseEntity.badRequest().body(new Response<>("user not found!", null));
+        }
+
+        Optional<Note> noteOpt = noteRepository.getByIdAndUserId(id, user.get().getId());
+
+        if (!noteOpt.isPresent()) {
             return ResponseEntity.badRequest().body(new Response<>("note not found!", null));
         }
-        Note note = notePromise.get();
+
+        Note note = noteOpt.get();
+
         note.setContent(noteRequest.getContent());
         note.setIsCompleted(noteRequest.getIsCompleted());
         noteRepository.save(note);
-        return ResponseEntity.ok(new Response<>("Success", note));
+
+        return ResponseEntity.ok(new Response<>("note updated!", note));
     }
 
     @Autheticate
     @DeleteMapping("/{id}")
-    @Operation(summary = "Delete note by id", description = "Delete note of user", tags = { "note" })
+    @Operation(summary = "Delete User Note", tags = { "note" })
     @ApiResponses(value = {
         @ApiResponse(responseCode = "204", description = "successful operation"),
         @ApiResponse(responseCode = "401", description = "failed operation",
                 content = @Content(schema = @Schema(implementation = Response.class))) })
-    public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
+    public ResponseEntity<Response<Note>> deleteNote(@PathVariable("id") Long id) {
+        Optional<User> user = userRepository.findById((Integer)request.getAttribute("user_id"));
 
-        Optional<Note> notePromise = noteRepository.getByIdAndUserId(id, (Integer)request.getAttribute("user_id"));
-        if (!notePromise.isPresent()) {
+        if (!user.isPresent()) {
+            return ResponseEntity.badRequest().body(new Response<>("user not found!", null));
+        }
+
+        Optional<Note> noteOpt = noteRepository.getByIdAndUserId(id, user.get().getId());
+
+        if (!noteOpt.isPresent()) {
             return ResponseEntity.badRequest().body(new Response<>("note not found!", null));
         }
-        Note note = notePromise.get();
+
+        Note note = noteOpt.get();
         noteRepository.delete(note);
-        return ResponseEntity.ok().body(new Response<>("no content!", null));
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
