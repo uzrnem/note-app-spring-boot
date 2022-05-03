6a502d79301c548c1081b96be0e757a1a25cddc2
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2022-05-03 14:14:26 +0530

    Update NoteRepository.java

--- src/main/java/com/example/demo/repository/NoteRepository.java
+++ src/main/java/com/example/demo/repository/NoteRepository.java
@@ -1,8 +1,14 @@
 package com.example.demo.repository;
 
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 import com.example.demo.entity.Note;
 
 public interface NoteRepository extends CrudRepository<Note, Integer> {
+    List<Note> getByUserId(Integer userId);
 
+    Optional<Note> getByIdAndUserId( Integer id, Integer userId);    
 }
